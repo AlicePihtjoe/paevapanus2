f0e3b811df494274bdd3e3bac0629c8a7a333a53
Alice Pihtjõe <alice.pihtjoe@khk.ee>
2023-06-09 15:26:21 +0300

    WIP added new file

---
+++ pages/api/topics.js
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        // Create a new topic
+        const { name, description, userId } = req.body;
+
+        try {
+            const topic = await prisma.topic.create({
+                data: {
+                    name,
+                    description,
+                    user: { connect: { id: userId } },
+                },
+            });
+
+            res.status(201).json(topic);
+        } catch (error) {
+            console.error('Error creating topic:', error);
+            res.status(500).json({ error: 'Failed to create topic' });
+        }
+    } else if (req.method === 'DELETE') {
+        // Delete a topic
+        // const name = Number(req.query.name);
+        const { topic } = req.query;
+
+        try {
+            const deletedTopic = await prisma.topic.delete({
+                where: {
+                    name: topic
+                },
+            });
+
+            res.status(200).json(deletedTopic);
+        } catch (error) {
+            console.error('Error deleting topic:', error);
+            res.status(500).json({ error: 'Failed to delete topic' });
+        }
+    } else {
+        res.status(404).json({ error: 'Not found' });
+    }
+}
